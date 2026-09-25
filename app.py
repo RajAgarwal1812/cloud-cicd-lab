bdbc6fac683e3771ea879fde64fa728c85334ccb
RajAgarwal1812 <agarwal.raj182005@gmail.com>
2026-09-25 16:54:31 +0530

    Test 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Version 1"
+    return "CI/CD Pipeline Version 2"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
